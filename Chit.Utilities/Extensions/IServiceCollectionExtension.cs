5589fa1ba29dc3d8246c17268cfb517654b7c134
adelowomi <adelowomi@gmail.com>
2023-11-01 15:46:58 +0100

    updated sendgrid and appsettings

--- Chit.Utilities/Extensions/IServiceCollectionExtension.cs
+++ Chit.Utilities/Extensions/IServiceCollectionExtension.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SendGrid.Extensions.DependencyInjection;
 
 namespace Chit.Utilities;
 
 public static class IServiceCollectionExtension
 {
-    public static IServiceCollection AddUtilities(this IServiceCollection services)
+    public static IServiceCollection AddUtilities(this IServiceCollection services, ConfigurationManager Configuration)
     {
-        
+        var notificationSettingsSection = Configuration.GetSection("NotificationConfig");
+        services.Configure<NotificationConfig>(notificationSettingsSection);
+
+        var config = notificationSettingsSection.Get<NotificationConfig>();
+
         services.AddMvc(options =>
         {
             options.Filters.Add<LinkRewritingFilter>();
+        });
+        services.AddSendGrid(options =>
+        {
+            options.ApiKey = config.SendGridApiKey;
         });
         services.AddTransient<IEmailHandler, EmailHandler>();
         return services;
